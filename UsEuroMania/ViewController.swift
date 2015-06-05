b6c502c9da9a145066f693e3d14ced298ccde9bb
Emma Koszinowski <emkosz@gmail.com>
2015-06-04 18:06:25 -0700

    Added conversion for bodyLength and travelDistance

--- UsEuroMania/ViewController.swift
+++ UsEuroMania/ViewController.swift
@@ -13,23 +13,80 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var bodyWeightTextField: UITextField!
     
+    @IBOutlet weak var bodyLenghtTextField: UITextField!
+    
+    @IBOutlet weak var travelDistanceTextField: UITextField!
+    
+    
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        println("----")
-        println("current string: \(bodyWeightTextField.text)")
-        println("replacement range: \(range)")
-        println("new stuff: \(string)")
-        let currentStringAsNSString = NSString(string: bodyWeightTextField.text)
         
-        let updatedString = currentStringAsNSString.stringByReplacingCharactersInRange(range, withString: string)
-        println("updated string: \(updatedString)")
         
-     
-        let bodyWeight = (updatedString as NSString).doubleValue
-        let bodyWeightInPounds = bodyWeight * 2.20462262
-        enteredValue.text = String(format: "%.1f", arguments:[bodyWeightInPounds])
-
+        // BODY WEIGHT
+        
+        if textField == bodyWeightTextField {
+            
+            println("----")
+            println("current string: \(bodyWeightTextField.text)")
+            println("replacement range: \(range)")
+            println("new stuff: \(string)")
+            
+            // transfer the input to NSString
+            let currentStringAsNSString = NSString(string: bodyWeightTextField.text)
+            
+            // store the updated string
+            let updatedString = currentStringAsNSString.stringByReplacingCharactersInRange(range, withString: string)
+            println("updated string: \(updatedString)")
+            
+            // transfer the string to a duouble
+            let bodyWeight = (updatedString as NSString).doubleValue
+            // the unit conversion
+            let bodyWeightInPounds = bodyWeight * 2.20462262
+            // put the converted value in the label, by converting it to a string, with one decimal
+            enteredValue.text = String(format: "%.1f", arguments:[bodyWeightInPounds])
+        
+            
+        }
+            
+        // BODY LENGHT
+        
+        else if textField == bodyLenghtTextField {
+            
+            // transfer the input to NSString
+            let currentLenghtStringAsNSString = NSString(string: bodyLenghtTextField.text)
+            
+            // store the updated string
+            let updatedLenghtString = currentLenghtStringAsNSString.stringByReplacingCharactersInRange(range, withString: string)
+            println("updated string: \(updatedLenghtString)")
+            
+            // transfer the string to a duouble
+            let bodyLenght = (updatedLenghtString as NSString).doubleValue
+            // the unit conversion
+            let bodyLenghtInFeet = bodyLenght * 0.032808399
+            // put the converted value in the label, by converting it to a string, with one decimal
+            enteredValueLenght.text = String(format: "%.1f", arguments:[bodyLenghtInFeet])
+            
+        }
             
         
+        // TRAVEL DISTANCE
+            
+        else if textField == travelDistanceTextField {
+            // transfer the input to NSString
+            let currentTravelDistanceStringAsNSString = NSString(string: travelDistanceTextField.text)
+            
+            // store the updated string
+            let updatedTravelString = currentTravelDistanceStringAsNSString.stringByReplacingCharactersInRange(range, withString: string)
+            println("updated string: \(updatedTravelString)")
+            
+            // transfer the string to a duouble
+            let travelDistance = (updatedTravelString as NSString).doubleValue
+            // the unit conversion
+            let travelDistanceInMiles = travelDistance * 0.621371192
+            // put the converted value in the label, by converting it to a string, with one decimal
+            enteredValueTravelDistance.text = String(format: "%.1f", arguments:[travelDistanceInMiles])
+            
+                
+        }
         
         
         
@@ -39,6 +96,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var enteredValue: UILabel!
+    
+    @IBOutlet weak var enteredValueLenght: UILabel!
+    
+    @IBOutlet weak var enteredValueTravelDistance: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -51,19 +113,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func buttonClicked(sender: AnyObject) {
-        
-        bodyWeightTextField.resignFirstResponder();
-        enteredValue.text = bodyWeightTextField.text;
-        
-    }
-    
-  
-    @IBAction func weightEntered(sender: UITextField) {
-        bodyWeightTextField.resignFirstResponder();
-        enteredValue.text = bodyWeightTextField.text;
-    }
     
   
 
